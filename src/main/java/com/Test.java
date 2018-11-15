a5b011350494b5b2e7e2e0b4843e801012120e9e
AlexandrVN <alexandr.novohatko@gmail.com>
2018-11-15 22:30:13 +0200

    refactored locale

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -13,81 +13,37 @@ Good night, World! в 23:00 - 06:00
 package com;
 
 import java.io.UnsupportedEncodingException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Test {
 
-    //private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH");
-
-
     public static void main(String[] args) {
-
-
-
-
-        //LocalDateTime now = LocalDateTime.now();
-
-        //String hour = DATE_TIME_FORMATTER.format(now);
-        //System.out.println(now);
-        //System.out.println(DATE_TIME_FORMATTER.format(now));
-
-        /*
-        1 - morning
-        2 - day
-        3 - evening
-        4 - night
-         */
 
         Date date = new Date();
         int hour = date.getHours();
-
-        String time;
-        switch (hour){
-            case 6:
-            case 7:
-            case 8:
-                time = "h1";
-                break;
-            case 9:
-            case 10:
-            case 11:
-            case 12:
-            case 13:
-            case 14:
-            case 15:
-            case 16:
-            case 17:
-            case 18:
-                time = "h2";
-                break;
-            case 19:
-            case 20:
-            case 21:
-            case 22:
-                time = "h3";
-                break;
-            default:
-                time = "h4";
-                break;
+        String ave;
+        if (hour>=6 && hour<9){
+            ave = "ave1";
+        } else if (hour>=9 && hour<19){
+            ave = "ave2";
+        } else if (hour>=19 && hour<23){
+            ave = "ave3";
+        } else {
+            ave = "ave4";
         }
 
-        Locale currentLocale = new Locale("en", "US");
+        //Locale currentLocale = new Locale("de");
+        Locale currentLocale = Locale.getDefault();
         ResourceBundle rb = ResourceBundle.getBundle("LabelsBundle", currentLocale);
-
         try {
-            String st = rb.getString(time);
+            String st = rb.getString(ave);
             String s1 = new String(st.getBytes("ISO-8859-1"),"UTF-8");
             System.out.println(s1);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-
-        //System.out.println(message);
-
 
     }
 }
