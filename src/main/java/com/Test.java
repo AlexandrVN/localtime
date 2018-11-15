42493baac6c30736b58b6aa0fad8df73178448cc
AlexandrVN <alexandr.novohatko@gmail.com>
2018-11-15 16:45:45 +0200

    added locate

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -14,18 +14,76 @@ package com;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Test {
 
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH");
+    //private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH");
+
 
     public static void main(String[] args) {
 
         System.out.println(Locale.getDefault());
 
-        LocalDateTime now = LocalDateTime.now();
-        System.out.println(DATE_TIME_FORMATTER.format(now));
+        Locale current = Locale.getDefault();
+        Locale myLocale = new Locale("ru", "UA");
+
+        Locale currentLocale = new Locale("by");
+        ResourceBundle rb = ResourceBundle.getBundle("LabelsBundle", currentLocale);
+
+        String st = rb.getString("h1");
+
+        System.out.println(st);
+
+        //LocalDateTime now = LocalDateTime.now();
+
+        //String hour = DATE_TIME_FORMATTER.format(now);
+        //System.out.println(now);
+        //System.out.println(DATE_TIME_FORMATTER.format(now));
+
+        /*
+        1 - morning
+        2 - day
+        3 - evening
+        4 - night
+         */
+
+        Date date = new Date();
+        int hour = date.getHours();
+        String message;
+
+        switch (hour){
+            case 6:
+            case 7:
+            case 8:
+                message = "Good morning, World!";
+                break;
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+                message = "Good day, World!";
+                break;
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+                message = "Good evening, World!";
+                break;
+            default:
+                message = "Good night, World!";
+                break;
+        }
+
+        System.out.println(message);
 
 
     }
